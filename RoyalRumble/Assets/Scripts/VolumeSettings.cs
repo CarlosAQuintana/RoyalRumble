c94fbe170a9d8af67c49c22d4388b5f3b0f2fe98
CarlosAQuintana <74573789+CarlosAQuintana@users.noreply.github.com>
2022-04-29 19:15:03 -0400

    SliderVolumeFix
    Sliders now remain at the same value throughout all scenes.

--- RoyalRumble/Assets/Scripts/VolumeSettings.cs
+++ RoyalRumble/Assets/Scripts/VolumeSettings.cs
@@ -23,21 +23,53 @@ public class VolumeSettings : MonoBehaviour
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
+    private void Start()
+    {
+        UpdateUIOnStart();
+    }
+
+    private void UpdateUIOnStart()
+    {
+        UpdateSlider(masterSlider, mixerMaster);
+        UpdateSlider(musicSlider, mixerBGM);
+        UpdateSlider(sfxSlider, mixerSFX);
+    }
+
+    private void UpdateSlider(Slider uiSlider, string mixerSlider)
+    {
+        float mixerSliderDB = 0;
+
+        bool results = mixer.GetFloat(mixerSlider, out mixerSliderDB);
+
+        //float convertedValue = DecibelToLinear(mixerSliderDB);
+        mixerSliderDB = DecibelToLinear(mixerSliderDB);
+
+        uiSlider.value = mixerSliderDB;
+    }
+
+
+    private float DecibelToLinear(float dB)
+    {
+        float linear = Mathf.Pow(10.0f, dB / 20.0f);
+
+        return linear;
+    }
+
 
     void SetMasterVolume(float value)
     {
-        mixer.SetFloat(mixerMaster, Mathf.Log10(value) * 10);
+        mixer.SetFloat(mixerMaster, Mathf.Log10(value) * 20);
     }
 
     void SetMusicVolume(float value)
     {
-        mixer.SetFloat(mixerBGM, Mathf.Log10(value) * 10);
+        mixer.SetFloat(mixerBGM, Mathf.Log10(value) * 20);
 
     }
 
     void SetSFXVolume(float value)
     {
-        mixer.SetFloat(mixerSFX, Mathf.Log10(value) * 10);
+        mixer.SetFloat(mixerSFX, Mathf.Log10(value) * 20);
 
     }
 }
